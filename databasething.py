1beb82868c01d93dc3aab5cadee582d779677d33
MentallyCoded <38444290+MentallyCoded@users.noreply.github.com>
2020-05-12 02:43:43 -0400

    Add files via upload

---
+++ databasething.py
@@ -0,0 +1,15 @@
+print('this is something i built in python')
+
+SmalldataBase = ('do you want to see what is in small database')
+
+smalldatabase = ['scdis.info','lost figures of ironbind','ashes of riv','corrupted file']
+
+print(smalldatabase[1])
+
+print(smalldatabase[2])
+
+print(smalldatabase[3])
+
+print(smalldatabase[0])
+
+print('task complete')
---
+++ databasething2.py
@@ -0,0 +1,40 @@
+import time
+
+class Garbage_Can:
+    pass
+
+garbageCan_1 = Garbage_Can()
+
+garbageCan_2 = Garbage_Can()
+
+
+x = 45 
+
+y = 80
+
+
+while x < 50 and y < 100:
+    x = x + 1
+    y = y + 1
+    print(garbageCan_1)
+    time.sleep(3)
+
+    print(Garbage_Can)
+    time.sleep(3)
+
+    print(garbageCan_2)
+    time.sleep(3)
+
+
+
+
+for x in range(0, 20):
+    print(garbageCan_1)
+
+    time.sleep(3)
+
+    print(Garbage_Can)
+
+    time.sleep(3)
+
+    print(garbageCan_2)
