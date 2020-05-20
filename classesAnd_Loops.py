03b385f99263e9a5928238b4bfb0dc2581930c01
MentallyCoded <38444290+MentallyCoded@users.noreply.github.com>
2020-05-20 16:58:47 -0400

    Add files via upload

---
+++ classesAnd_Loops.py
@@ -0,0 +1,51 @@
+import time
+
+class Garbage_Can:
+    pass
+class BREAK_IDLE:
+    pass
+
+BREAK_IDLE.ehlo = BREAK_IDLE()
+
+
+garbageCan_1 = Garbage_Can()
+
+garbageCan_2 = Garbage_Can()
+
+
+x = 45 
+
+y = 80
+
+
+while x < 50 and y < 100:
+    x = x + 1
+    y = y + 1
+    print(garbageCan_1)
+
+    
+
+    print(Garbage_Can)
+
+   
+
+    print(garbageCan_2)
+
+
+
+for x in range(0, 20):
+    print(garbageCan_1)
+
+    print(Garbage_Can)
+
+    print(garbageCan_2)
+
+
+for x in range(0, 5):
+
+    print('task complete')
+    print(BREAK_IDLE)
+
+for x in range(0, 20):
+
+    print(BREAK_IDLE)
